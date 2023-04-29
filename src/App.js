e0c671da26a69725e65a93751144fa95739556e8
abdullah-al-ryan <abdullah15-13088@diu.edu.bd>
2023-04-29 20:28:43 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import Pricing from './components/Pricing/Pricing';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+      <h1 className='text-4xl font-bold underline'>This is a H1 header</h1>
+      <p>This is just a paragraph</p>
+      <Pricing></Pricing>
+    </div>
+  );
+}
+
+export default App;
